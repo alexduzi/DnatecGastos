170299453c507b7e12a200a9a1a924367a4e0dbc
Duzi <Duzi>
2015-02-12 18:59:18 -0200

    alterações de authenticação

--- Dnatec_ControleGastos/app/forms.py
+++ Dnatec_ControleGastos/app/forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.utils.translation import ugettext_lazy as _
 
 class BootstrapAuthenticationForm(AuthenticationForm):
-    """Authentication form which uses boostrap CSS."""
+    
     username = forms.CharField(max_length=254,
                                widget=forms.TextInput({
                                    'class': 'form-control',
--- Dnatec_ControleGastos/app/views.py
+++ Dnatec_ControleGastos/app/views.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+
 
 from django.shortcuts import render
 from django.http import HttpRequest
@@ -6,15 +6,23 @@ from django.template import RequestContext
 from datetime import datetime
 
 def home(request):
-    
-    return render(
+
+    if  request.user.is_authenticated():
+        return render(
         request,
         'app/index.html',
         context_instance = RequestContext(request,
         {
             'title':'Bem vindo ao sistema de controle de gastos!',
-        })
-    )
+        }))   
+
+    return render(request,
+                'app/login.html',
+                context_instance = RequestContext(request,
+                {
+                    'title':'',
+                }))
+    
 
 def about(request):
     
@@ -24,7 +32,7 @@ def about(request):
         context_instance = RequestContext(request,
         {
             'title':'Controle de gastos',
-            'message':'Aplicação de controle de gastos do projeto DNATEC desenvolvido por Alex Duzi',
+            'message':'Aplicacao de controle de gastos do projeto DNATEC desenvolvido por Alex Duzi',
         })
     )
 
